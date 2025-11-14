fb8588e589957634cf2c3bc0670f1ed8427e4ee8
Mete <metebatirtunkiz@gmail.com>
2025-11-14 16:38:16 +0300

    Defining methods, calling methods, methods can use the same variable name as a variable in main method explained

---
+++ Chapter11_Methods/Chapter11_vsfile/Program.cs
@@ -0,0 +1,41 @@
+﻿
+// console title
+Console.Title = "Methods";
+
+Console.WriteLine("What is your current age?");
+string answer = Console.ReadLine();
+int current = Convert.ToInt32(answer); // using the same variable name, no error!!
+
+if(current < 18)
+{
+    Console.WriteLine($"You are not allowed to drive a car since you are {current}");
+}
+else
+{
+    Console.WriteLine("You can drive a car!");
+}
+
+
+CountToTen();
+CountToTwenty(); // calling the method
+
+// this is a definition of a method which is not enough to run this code block (for loop)
+// you should CALL a method to make this code block run
+void CountToTwenty() // defining a method ( basically creating a method)
+{
+    for (int current = 1; current <= 20; current++) // start counting from 1 to 20
+    {
+        Console.WriteLine(current);
+    }
+}
+void CountToTen()
+{
+    for(int current = 1; current <= 10; current++)
+    {
+        Console.WriteLine(current); // both methods and a variable use the same variable name with no error!!
+    }
+}
+
+CountToTwenty(); // this method can be called anywhere since it is a LOCAL FUNCTION
+//  CountToTwenty(); method can be called from anywhere because it lives in our MAIN Method
+
